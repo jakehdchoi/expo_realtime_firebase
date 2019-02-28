ef38269308ce325adbbbc2ab349562a84a6f08cc
jakehdchoi <jakehdchoi@gmail.com>
2019-02-28 22:07:56 +0900

    it works

--- App.js
+++ App.js
@@ -1,11 +1,93 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList
+} from "react-native";
+import { Constants } from "expo";
+import firebase from "firebase";
+import config from "./config";
+
+firebase.initializeApp(config);
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: "",
+      messages: []
+    };
+
+    this.addItem = this.addItem.bind(this);
+  }
+
+  componentDidMount() {
+    firebase
+      .database()
+      .ref()
+      .child("messages")
+      .once("value", snapshot => {
+        const data = snapshot.val();
+        if (snapshot.val()) {
+          const initMessages = [];
+          Object.keys(data).forEach(message =>
+            initMessages.push(data[message])
+          );
+          this.setState({
+            messages: initMessages
+          });
+        }
+      });
+
+    firebase
+      .database()
+      .ref()
+      .child("messages")
+      .on("child_added", snapshot => {
+        const data = snapshot.val();
+        if (data) {
+          this.setState(prevState => ({
+            messages: [data, ...prevState.messages]
+          }));
+        }
+      });
+  }
+
+  addItem() {
+    if (!this.state.message) return;
+
+    const newMessage = firebase
+      .database()
+      .ref()
+      .child("messages")
+      .push();
+    newMessage.set(this.state.message, () => this.setState({ message: "" }));
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.msgBox}>
+          <TextInput
+            placeholder="Enter your message"
+            value={this.state.message}
+            onChangeText={text => this.setState({ message: text })}
+            style={styles.txtInput}
+          />
+          <Button title="Send" onPress={this.addItem} />
+        </View>
+        <FlatList
+          data={this.state.messages}
+          renderItem={({ item }) => (
+            <View style={styles.listItemContainer}>
+              <Text style={styles.listItem}>{item}</Text>
+            </View>
+          )}
+        />
       </View>
     );
   }
@@ -14,8 +96,24 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#eee",
+    marginTop: Constants.statusBarHeight
   },
+  msgBox: {
+    flexDirection: "row",
+    padding: 20,
+    backgroundColor: "#fff"
+  },
+  txtInput: {
+    flex: 1
+  },
+  listItemContainer: {
+    backgroundColor: "#fff",
+    margin: 5,
+    borderRadius: 5
+  },
+  listItem: {
+    fontSize: 20,
+    padding: 10
+  }
 });
